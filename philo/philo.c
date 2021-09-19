a7f7223facd2c77d51c7993b0a334ed3b55ab751
devleo <wltnskjs@naver.com>
2021-09-19 13:13:27 +0900

    add

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:37:35 by jisokang          #+#    #+#             */
-/*   Updated: 2021/09/13 15:55:39 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/09/18 18:01:37 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,22 @@ static int	error_msg(char *str)
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	argv_num[5];
+	int		i;
+	int		argv_num[5];
+	t_info	*info;
 
 	if (argc < 5 || argc > 6)
 	{
 		printf("Wrong argument! Please check usage.\n"\
 			YELLOW"Usage: ./philo [num_philos] [time_die] [time_eat]\
-			[time_sleep] [num_each_philos_eat(option)]\n"RESET);
+			[time_sleep] [num_each_philos_eat(optional)]\n"RESET);
 		return (ERROR);
 	}
 	i = 1;
 	while (i < argc)
 	{
-		//atoi -> ft_atoi
+		//atoi -> ft_atoi for philo -> 음수 처리
+		//
 		argv_num[i] = atoi(argv[i]);
 		printf("argv_num[%d] = %d\n", i, argv_num[i]);
 		i++;
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 13:54:15 by jisokang          #+#    #+#             */
-/*   Updated: 2021/09/13 14:56:02 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/09/19 13:04:40 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,23 @@
 # define ERROR	1
 # define TRUE	1
 # define FALSE	0
+
+typedef struct s_philo
+{
+	int	num;
+	int	stat;
+}		t_philo;
+
+typedef struct s_info
+{
+	int				num_philo;
+	int				time_die;
+	int				time_eat;
+	int				time_sleep;
+	int				num_phi_eat;
+	pthread_mutex_t	die_mutex;
+	pthread_mutex_t	_mutex;
+	t_philo			*philo;
+}					t_info;
 
 #endif
